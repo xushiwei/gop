dfc121933667f21bba3a9b882c61db0e67575b6c
xushiwei <x@goplus.org>
2026-03-15 17:48:53 +0800

    xgo builtin func: contains

--- cl/_testgop/cap/out.go
+++ cl/_testgop/cap/out.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/qiniu/x/stringslice"
+	"github.com/qiniu/x/stringutil"
 )
 
 func main() {
 	a := []string{"hello", "world", "123"}
 	fmt.Println(stringslice.Capitalize(a))
+	fmt.Println(stringutil.Contains("param-required required", "required"))
+	fmt.Println(stringutil.Contains("param-required required", "param"))
 }
