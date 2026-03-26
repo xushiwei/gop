b93741341df740407326d8c2de8fa4230367e8a4
xushiwei <x@goplus.org>
2026-03-26 09:14:33 +0800

    mv testcases to _testgop

---
+++ cl/_testgop/overload-func1/out.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func add__0(a int, b int) int {
+	return a + b
+}
+func add__1(a string, b string) string {
+	return a + b
+}
+func main() {
+	fmt.Println(add__0(100, 7))
+	fmt.Println(add__1("Hello", "World"))
+}
